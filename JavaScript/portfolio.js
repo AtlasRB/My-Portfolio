4eabd1171da925b308a765beb27096bc42cb7050
Connor Yeeles <connoryeeles@outlook.com>
2024-01-31 11:04:32 +0000

    Added json file with portfolio info

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -1,5 +1,23 @@
+//Functions
 function toggleDropdown() {
     document.querySelector('.dropdown').classList.toggle('visible')
 }
 
+const portfolioTarget = document.querySelector('.items')
+
+fetch('JavaScript/portfolio.json').then(function (response) {
+    return response.json()
+}).then(function (data) {
+    console.log(data)
+    data.Portfolio.forEach(function (item) {
+        portfolioTarget.innerHTML += `<div>
+            <h3 class="title">${item.name}</h3>
+            <p>${item.img}</p>
+            <p>${item.description}</p>
+            <p>${item.link}</p>
+         </div>`
+    })
+}) 
+
+//Call functions
 document.querySelector('.drop').addEventListener('click', toggleDropdown)
