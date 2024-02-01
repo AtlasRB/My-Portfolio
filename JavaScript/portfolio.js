9cc26b3c6e6a622d67b3bb4178c1335e3a5c108a
Connor Yeeles <connoryeeles@outlook.com>
2024-02-01 15:27:55 +0000

    Added dropdown content for extra project info

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -8,17 +8,33 @@ const portfolioTarget = document.querySelector('.items')
 fetch('JavaScript/portfolio.json').then(function (response) {
     return response.json()
 }).then(function (data) {
+    let counter = 1
     data.Portfolio.forEach(function (item) {
         portfolioTarget.innerHTML += `<div class="project">
             <h3 class="title">${item.name}</h3>
             <div class = "content">
                 <img src="${item.img}" class="imagewindow" alt="Placeholder text"/>
                 <p>${item.description}</p>
-                <a href="${item.link} class="" target="_blank">GitHub ${item.name}</a>
+                <div class="clicker">
+                    <a href="${item.link} class="" target="_blank">GitHub ${item.name}</a>
+                    <p>More details <i data-opens=".project${counter}" class="fa-solid fa-caret-down moreDrop"></i></p>
+                </div>
             </div>
+            <p class="more project${counter}">${item.largeDescription}</p> 
          </div>`
+         counter += 1
     })
+    const buttons = document.querySelectorAll('.moreDrop')
+    buttons.forEach(function (target) {
+        target.addEventListener('click', function () {
+            const buttonOpen = target.dataset.opens
+            const selection = document.querySelector(buttonOpen)
+            selection.classList.toggle('visible')
+        })
+    })
+
 }) 
 
 //Call functions
 document.querySelector('.drop').addEventListener('click', toggleDropdown)
+
